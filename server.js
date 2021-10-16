bc9d764b27ae71666fe209b2af0061dec681fb70
Glitch (fluttering-responsible-crop) <none>
2021-10-16 12:40:27 +0000

    🐷👘 Checkpoint ./server.js:37358078/109

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ app.post("/webhook", async (req, res) => {
   ) {
     // search for the laptop user needs
     for (let laptop in laptop_dict) {
-      console.log (laptop.name);
+      console.log (laptop);
       if (laptop.name == wit.entities["laptop_name:laptop_name"][0].value) {
         laptop_instance = laptop;
         break;
